e79bbe5cfdc25b4159f3b697b05800c9f448a163
andreamatt <andreamatte97@gmail.com>
2021-09-15 23:48:55 +0200

    simplified and unified dataloaders

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -3,21 +3,16 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Preprocess')
-parser.add_argument('--dataset', default='qnrf',
-                    help='dataset name, only support qnrf and nwpu')
-parser.add_argument('--input-dataset-path', default='data/QNRF',
-                    help='original data directory')
-parser.add_argument('--output-dataset-path', default='data/QNRF-Train-Val-Test',
-                    help='processed data directory')
+parser.add_argument('--dataset', default='qnrf', help='dataset name, only support qnrf and nwpu')
+parser.add_argument('--input-dataset-path', default='data/QNRF', help='original data directory')
+parser.add_argument('--output-dataset-path', default='data/QNRF-Train-Val-Test', help='processed data directory')
 args = parser.parse_args()
 
 if args.dataset.lower() == 'qnrf':
-    from preprocess.preprocess_dataset_qnrf import main
-
-    main(args.input_dataset_path, args.output_dataset_path, 512, 2048)
+	from preprocess.preprocess_dataset_qnrf import main
+	main(args.input_dataset_path, args.output_dataset_path, 512, 2048)
 elif args.dataset.lower() == 'nwpu':
-    from preprocess.preprocess_dataset_nwpu import main
-
-    main(args.input_dataset_path, args.output_dataset_path, 384, 1920)
+	from preprocess.preprocess_dataset_nwpu import main
+	main(args.input_dataset_path, args.output_dataset_path, 384, 1920)
 else:
-    raise NotImplementedError
+	raise NotImplementedError
